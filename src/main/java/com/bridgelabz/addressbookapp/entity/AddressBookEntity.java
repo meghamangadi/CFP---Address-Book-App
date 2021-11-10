58a9b62ea6b34add45545e498f218c1a151afc10
megha angadi <meghamangadi@gmail.com>
2021-11-10 23:00:13 +0530

    Added : Lombok Library for Model Response and Entity Classes

--- src/main/java/com/bridgelabz/addressbookapp/entity/AddressBookEntity.java
+++ src/main/java/com/bridgelabz/addressbookapp/entity/AddressBookEntity.java
@@ -4,10 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
+ 
  
 
  
-@Entity 
+@Entity
+@Data
 public class AddressBookEntity {
 	
 		@Id
@@ -19,54 +22,6 @@ public class AddressBookEntity {
 	    private String state;
 	    private String city;
 	    private String zip;
-	    private String phone;
-		public Long getContactId() {
-			return contactId;
-		}
-		public void setContactId(Long contactId) {
-			this.contactId = contactId;
-		}
-		public String getFirstName() {
-			return firstName;
-		}
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
-		public String getLastName() {
-			return lastName;
-		}
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
-		public String getAddress() {
-			return address;
-		}
-		public void setAddress(String address) {
-			this.address = address;
-		}
-		public String getState() {
-			return state;
-		}
-		public void setState(String state) {
-			this.state = state;
-		}
-		public String getCity() {
-			return city;
-		}
-		public void setCity(String city) {
-			this.city = city;
-		}
-		public String getZip() {
-			return zip;
-		}
-		public void setZip(String zip) {
-			this.zip = zip;
-		}
-		public String getPhone() {
-			return phone;
-		}
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
+	    private String phone;		 
 	    
 }
--- src/main/java/com/bridgelabz/addressbookapp/model/ContactInfo.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/ContactInfo.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.addressbookapp.model;
 
+import lombok.Data;
+
+@Data
 public class ContactInfo {
 
 	private String firstName;
@@ -8,48 +11,5 @@ public class ContactInfo {
 	private String state;
 	private String city;
 	private String zip;
-	private String phone;
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getZip() {
-		return zip;
-	}
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	
+	private String phone; 
 }
